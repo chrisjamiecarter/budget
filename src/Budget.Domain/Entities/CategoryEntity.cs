32a12bacb6676077c7ae34bd2947a8c97eb28823
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2024-08-29 21:15:30 +0100

    Added Domain Entities

---
+++ src/Budget.Domain/Entities/CategoryEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace Budget.Domain.Entities;
+
+public class CategoryEntity
+{
+    #region Properties
+
+    public Guid Id { get; set; }
+
+    public string? Name { get; set; }
+
+    #endregion
+}
---
+++ src/Budget.Domain/Entities/TransactionEntity.cs
@@ -0,0 +1,18 @@
+﻿namespace Budget.Domain.Entities;
+
+public class TransactionEntity
+{
+    #region Properties
+
+    public Guid Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public DateTime Date { get; set; }
+
+    public decimal Amount { get; set; }
+
+    public CategoryEntity? Category { get; set; }
+
+    #endregion
+}
